e4c548eadec5478b10ce4f8753ba228a2f22dd4f
Han4578 <99018603+Han4578@users.noreply.github.com>
2022-11-13 16:47:04 +0800

    able to solve medium difficulty
    nakedsingle and hiddensingle search methods done

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 import preset from "./medium.json" assert {type: 'json'} 
 
 let board = document.querySelector('.board')
+let start = document.getElementById('start')
 let tiles = []
+let bigTiles = []
 
 for (let a = 1; a <= 9; a++) {
     let bigTile = document.createElement('div')
@@ -31,38 +33,92 @@ for (let a = 1; a <= 9; a++) {
         }
     }
     board.appendChild(bigTile)
+    bigTiles.push(bigTile)
 }
 
 preset.forEach(n => {
     n.location.forEach(l => {
         let tile = document.getElementById(l)
         tile.innerText = n.value
+        tile.classList.add('preset')
     })
 })
 
-let movesMade = true
+let movesMade = 1
 let emptyTiles
-while(movesMade){
-    movesMade = false
-    checkForPossibleNumbers()
-    // singleFill()
+start.addEventListener('click', startSolve)
+function startSolve(){
+    while (movesMade > 0) {
+        movesMade = 0
+        findNakedSingles()
+        findHiddenSingles()
+    }
 }
 
-function checkForPossibleNumbers() {
+function updateEmptyTiles() {
     emptyTiles = tiles.filter(t => { //get all empty tiles
         return t.innerText == ''
     })
+    checkForPossibleNumbers()
+}
+
+function checkForPossibleNumbers() {
 
     emptyTiles.forEach(et => { // repeat for every tile
         let adjacentTiles = tiles.filter(t => { //get all the tiles that are horizontal/vertical/same big square to the empty tile and not empty
             return ((t.id[1] == et.id[1]) || (t.id[0] == et.id[0]) || (t.parentElement.id == et.parentElement.id)) && (t.id !== et.id) && t.innerText !== ''
         })
-        let numbers = [...new Set(adjacentTiles.map(t => {return t.innerText}))] // get their innertext 
-        let original = [1,2,3,4,5,6,7,8,9]
+        let numbers = [...new Set(adjacentTiles.map(t => {
+            return t.innerText
+        }))] // get their innertext 
+        let original = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
         let possibleNum = original.filter(n => { // get all numbers that are not in numbers array
             return !numbers.includes(n.toString())
         })
         et.dataset.possibleNum = possibleNum // add to tile dataset
+    })
+}
+
+function findNakedSingles() {
+    updateEmptyTiles()
+    let singleTiles = emptyTiles.filter(t => {
+        return t.dataset.possibleNum.length == 1
+    })
+    if (singleTiles.length > 0) {
+        singleTiles.forEach(t => {
+            t.innerText = t.dataset.possibleNum;
+            movesMade++
+        })
+    }
+}
+
+function findHiddenSingles() {
+    updateEmptyTiles()
+    bigTiles.forEach(bt => {
+        let allValues = []
+        let empty = emptyTiles.filter(t => {
+            return t.parentElement == bt
+        })
+        empty.forEach(t => {
+            let num = [...t.dataset.possibleNum]
+            num.forEach(n => {
+                if (!isNaN(n)) {
+                    allValues.push(n)
+                }
+            })
+        })
+        let count = {}
+        allValues.forEach(n => {count[n] = (count[n] || 0) + 1})
+        for (const key in count) {
+            if (count[key] == 1) {
+                empty.forEach(t => {
+                    if (t.dataset.possibleNum.includes(key)) {
+                        t.innerText = key
+                        movesMade++
+                    }
+                });
+            }
+        }
     })
 }
