7e0df274a7dbfbb8c1b918f2fc605f6cff391a42
Han4578 <99018603+Han4578@users.noreply.github.com>
2022-11-26 18:40:15 +0800

    renamed maps

--- stages/index.js
+++ stages/index.js
@@ -1,4 +1,10 @@
+let medium = [M1]
+let hard = [H1]
+let evil = [E1, E2]
+
+import {default as M1} from "./M1.json" assert {type: "json"}
+import {default as H1} from "./H1.json" assert {type: "json"}
+import {default as E1} from "./E1.json" assert {type: "json"}
+import {default as E2} from "./E2.json" assert {type: "json"}
+
 export const stages = [medium, hard, evil]
-import {default as medium} from "./medium.json" assert {type: "json"}
-import {default as hard} from "./hard.json" assert {type: "json"}
-import {default as evil} from "./evil.json" assert {type: "json"}
