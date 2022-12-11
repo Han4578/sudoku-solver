e8a736528d767006e196c40941f2e6156edde2da
Han4578 <99018603+Han4578@users.noreply.github.com>
2022-12-11 18:47:13 +0800

    added new maps

--- stages/index.js
+++ stages/index.js
@@ -1,9 +1,11 @@
-let medium = [M1]
-let hard = [H1]
+let medium = [M1, M2]
+let hard = [H1, H2]
 let evil = [E1, E2]
 
 import {default as M1} from "./M1.json" assert {type: "json"}
+import {default as M2} from "./M2.json" assert {type: "json"}
 import {default as H1} from "./H1.json" assert {type: "json"}
+import {default as H2} from "./H2.json" assert {type: "json"}
 import {default as E1} from "./E1.json" assert {type: "json"}
 import {default as E2} from "./E2.json" assert {type: "json"}
 
